178f00393d2894c43494e43ab4ae3bffe52c23a7
Bienaime <bybienaime@gmail.com>
2019-10-27 08:10:47 +0200

    ft(server): add welcome route
    create a welcome route
    handle wrong routes errors
    add tests of those routes
    
    [Finishes #169382878]

--- server/app.js
+++ server/app.js
@@ -1,10 +1,42 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8000;
 
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+
+app.get('/', (_req, res) => {
+  res.status(200).send({
+    status: 200,
+    message:
+      'Welcome to MyDiary'
+  });
+});
+
+
+app.use('/*', (_req, res) => {
+  res.status(404).send({ status: 404, error: 'Route Not Found' });
+});
+
+app.use((error, _req, res, _next) => {
+  if (error.status === 400) {
+    return res.status(error.status).json({
+      error: {
+        message: 'Syntax error, Please double check your input'
+      }
+    });
+  }
+});
 
 app.listen(port);
 
